be429f0f08eacb341a8bea91a1d6a8e4059eafad
Ananya Chauhan <ambivertengineer@gmail.com>
2025-11-12 10:23:20 +0530

    read-router-dom and form handling

--- ReactWork/reactViteApp/src/App.jsx
+++ ReactWork/reactViteApp/src/App.jsx
@@ -1,19 +1,41 @@
-import {usecase } from 'react'
-import './App.css'
-import Login from './pages/login.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import "./App.css";
+import Login from "./pages/Login";
+// e.g. in src/main.jsx or src/App.jsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Registration from "./pages/Registration";
+import MainLayout from "./pages/MainLayout";
+import { useState } from "react";
 function App() {
+  const [data, setData] = useState();
+  console.log(data);
+  
   return (
     <div>
+      {/* <h1 className="head">Welcome to React</h1> */}
+      {/* <h2 className="profile-head">Profile Page</h2>
+
+      <Gallery />
+      <StateHandling /> */}
+      {/* <ImageManipulation /> */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/login" element={<Login/>} />
-        </Routes> 
+          {/* <Route path="/" element={<Gallery />} />
+          <Route path="/state" element={<StateHandling />} />
+          <Route path="/image" element={<ImageManipulation />} /> */}
+          <Route path="/login" element={<Login logData={data}/>} />
+          <Route
+            path="/register"
+            element={<Registration regData={setData} />}
+          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<MainLayout />} />
+        </Routes>
       </BrowserRouter>
+      <h2>{JSON.stringify(data)}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
